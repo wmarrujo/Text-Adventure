4ac1f4e8a091e68156aa2eba10da999be3c2d5ea
gmarimon <gmarimon@purdue.edu>
2016-01-31 18:13:41 -0500

    Started working on Things class and respective subclasses

--- main.swift
+++ main.swift
@@ -6,3 +6,16 @@ dump(args)
 
 print("test")
 print("changed")
+
+
+var thing = Thing()
+var item = Item()
+var weapon = Weapon()
+var food = Food()
+var furniture = Furniture()
+var container = Container()
+var location = Location()
+var position = Position()
+var creature = Creature()
+var player = Player()
+var npc = NPC()
